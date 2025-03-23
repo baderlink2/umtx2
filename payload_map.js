94de8abd5bfe42a0e097cd741b907e4a25c50208
baderlink2 <bouazzaelharyzy@gmail.com>
2025-03-23 14:05:52 +0100

    Add files via upload

--- payload_map.js
+++ payload_map.js
@@ -53,6 +53,17 @@ const payload_map = [
         supportedFirmwares: ["1.", "2."],
         toPort: 9021
     },
+	{
+        displayTitle: "etaHEN", 
+        description: "AIO HEN",
+        fileName: "etaHENByLM.bin",
+        author: "LightningMods",
+        projectSource: "https://github.com/LightningMods/etaHEN",
+        binarySource: "https://github.com/LightningMods/etaHEN/releases/download/1.9b/etaHEN.bin",
+        version: "2.0b",
+		supportedFirmwares: ["5."],
+		toPort: 9021
+    },
     {
         displayTitle: "ps5-kstuff",
         description: "FPKG enabler",
