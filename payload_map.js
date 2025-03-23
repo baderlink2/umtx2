04f2f12c4d086a6327943997d12914f11cde0f98
baderlink2 <bouazzaelharyzy@gmail.com>
2025-03-23 14:39:13 +0100

    Add files via upload

--- payload_map.js
+++ payload_map.js
@@ -54,15 +54,15 @@ const payload_map = [
         toPort: 9021
     },
     {
-        displayTitle: "ps5-kstuff",
-        description: "FPKG enabler",
-        fileName: "kstuff.elf",
-        author: "sleirsgoevy, john-tornblom, EchoStretch, buzzer-re, BestPig, LightningMods, zecoxao",
-        projectSource: "https://github.com/EchoStretch/kstuff",
-        binarySource: "https://github.com/EchoStretch/kstuff/releases/download/5xx-support-v1/kstuff.elf",
-        version: "081f53b",
-        supportedFirmwares: ["3.", "4.", "5."],
-        toPort: 9021
+        displayTitle: "Etahen+Kstuff", 
+        description: "For 5.50",
+        fileName: "etahen_kstuff.bin",
+        author: "LightningMods",
+        projectSource: "https://github.com/LightningMods/etaHEN",
+        binarySource: "https://github.com/LightningMods/etaHEN/releases/download/1.9b/etaHEN.bin",
+        version: "2.0b",
+		supportedFirmwares: ["5.50"],
+		toPort: 9021
     },
     {
         displayTitle: "Byepervisor HEN",
@@ -106,14 +106,15 @@ const payload_map = [
         toPort: 9021
     },
     {
-        displayTitle: "klogsrv",
-        description: "Klog server. Runs on port 3232.",
-        fileName: "klogsrv.elf",
-        author: "john-tornblom",
-        projectSource: "https://github.com/ps5-payload-dev/klogsrv",
-        binarySource: "https://github.com/ps5-payload-dev/pacbrew-repo/actions/runs/13686166926",
-        version: "0.5.2",
-        toPort: 9021
+        displayTitle: "Etahen+Kstuff", 
+        description: "For 5.10",
+        fileName: "etahen_kstuff10.bin",
+        author: "LightningMods",
+        projectSource: "https://github.com/LightningMods/etaHEN",
+        binarySource: "https://github.com/LightningMods/etaHEN/releases/download/1.9b/etaHEN.bin",
+        version: "2.0b",
+		supportedFirmwares: ["5.10"],
+		toPort: 9021
     },
     {
         displayTitle: "shsrv",
