f6e9f106ce33526ec5318dd67e222ec3ad99f432
baderlink2 <bouazzaelharyzy@gmail.com>
2025-04-01 00:29:14 +0200

    Update payload_map.js

--- payload_map.js
+++ payload_map.js
@@ -33,20 +33,20 @@ const payload_map = [
     // etaHEN is added twice so that on 1.xx-2.xx you can load it in webkit only mode too
     // but on 3.xx-4.xx it only shows in kernel exploit mode since it needs the 9020 elf loader for kstuff
     {
-        displayTitle: "etaHEN",
-        description: "AIO HEN",
+        displayTitle: "Etahen+Kstuff NEW Last",
+        description: "For 3.XX-4.XX",
         fileName: "etaHEN.bin",
-        author: "LightningMods, Buzzer, sleirsgoevy, ChendoChap, astrelsky, illusion, CTN, SiSTR0, Nomadic",
+        author: "LightningMods Date 30-03-2025",
         projectSource: "https://github.com/LightningMods/etaHEN",
         binarySource: "https://github.com/LightningMods/etaHEN/releases/download/1.9b/etaHEN.bin",
         version: "1.9b",
         supportedFirmwares: ["3.", "4."]
     },
     {
-        displayTitle: "etaHEN",
-        description: "AIO HEN",
+        displayTitle: "Etahen+Kstuff NEW Last",
+        description: "For 1.xx-2.xx",
         fileName: "etaHEN.bin",
-        author: "LightningMods, Buzzer, sleirsgoevy, ChendoChap, astrelsky, illusion, CTN, SiSTR0, Nomadic",
+        author: "LightningMods Date 30-03-2025",
         projectSource: "https://github.com/LightningMods/etaHEN",
         binarySource: "https://github.com/LightningMods/etaHEN/releases/download/1.9b/etaHEN.bin",
         version: "1.9b",
