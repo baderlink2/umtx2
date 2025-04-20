5ce091d1bf3c4bb453c32aef46b6bccd0262a336
baderlink2 <bouazzaelharyzy@gmail.com>
2025-04-20 04:04:45 +0200

    Update payload_map.js

--- payload_map.js
+++ payload_map.js
@@ -41,7 +41,7 @@ const payload_map = [
         toPort: 9021
     },
     {
-        displayTitle: "ONLY KSTUFF V1.3 LAST",
+        displayTitle: "ONLY KSTUFF V1.4 LAST",
         description: "FPKG PS4",
         fileName: "kstuff.elf",
         author: "sleirsgoevy, john-tornblom, EchoStretch, buzzer-re, BestPig, LightningMods, zecoxao",
