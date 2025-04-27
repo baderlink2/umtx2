fecdd44978a5c81103c1f318c3ccf36d237b6e2f
baderlink2 <bouazzaelharyzy@gmail.com>
2025-04-27 03:58:21 +0200

    Update payload_map.js

--- payload_map.js
+++ payload_map.js
@@ -37,7 +37,7 @@ const payload_map = [
         author: "Last UPDATE 27-04-2025",
         projectSource: "https://github.com/etaHEN/etaHEN",
         binarySource: "https://github.com/etaHEN/etaHEN/blob/360e7c0d2dddf4f4645599d51d652e707c0b717b/etaHEN-2.0b.bin",
-        version: "2.0b",
+        version: "2.1B",
         toPort: 9021
     },
     {
@@ -47,7 +47,7 @@ const payload_map = [
         author: "Last UPDATE 27-04-2025",
         projectSource: "https://github.com/EchoStretch/kstuff",
         binarySource: "https://github.com/EchoStretch/kstuff/releases/download/v1.3/kstuff.elf",
-        version: "1.3",
+        version: "1.4",
         supportedFirmwares: ["3.", "4.", "5."],
         toPort: 9021
     },
