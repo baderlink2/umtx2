0003a2f4d7d2bf56b618d7afae5f3a8542d184c4
baderlink2 <bouazzaelharyzy@gmail.com>
2025-04-27 05:39:03 +0200

    Update payload_map.js

--- payload_map.js
+++ payload_map.js
@@ -51,37 +51,8 @@ const payload_map = [
         supportedFirmwares: ["3.", "4.", "5."],
         toPort: 9021
     },
-    {
-        displayTitle: "Byepervisor HEN",
-        description: "FPKG enabler",
-        fileName: "byepervisor.elf",
-        author: "SpecterDev, ChendoChap, flatz, fail0verflow, Znullptr, kiwidog, sleirsgoevy, EchoStretch, LightningMods, BestPig, zecoxao", 
-        projectSource: "https://github.com/EchoStretch/Byepervisor",
-        binarySource: "https://github.com/EchoStretch/Byepervisor/actions/runs/14004003762",
-        version: "84164bb",
-        supportedFirmwares: ["1.00", "1.01", "1.02", "1.12", "1.14", "2.00", "2.20", "2.25", "2.26", "2.30", "2.50", "2.70"],
-        toPort: 9021
-    },
-    {
-        displayTitle: "libhijacker game-patch",
-        description: "Patches supported games to run at higher framerates, and adds debug menus to certain titles.",
-        fileName: "libhijacker-game-patch.v1.160.elf",
-        author: "illusion0001, astrelsky",
-        projectSource: "https://github.com/illusion0001/libhijacker",
-        binarySource: "https://github.com/illusion0001/libhijacker-game-patch/releases/tag/1.160-75ab26a3",
-        version: "1.160",
-        supportedFirmwares: ["3.", "4."]
-    },
-    {
-        displayTitle: "websrv",
-        description: "Custom homebrew loader. Runs on port 8080.",
-        fileName: "websrv.elf",
-        author: "john-tornblom",
-        projectSource: "https://github.com/ps5-payload-dev/websrv",
-        binarySource: "https://github.com/ps5-payload-dev/websrv/actions/runs/14318408868",
-        version: "0.22",
-        toPort: 9021
-    },
+
+    
     {
         displayTitle: "ftpsrv",
         description: "FTP server. Runs on port 2121.",
@@ -92,77 +63,7 @@ const payload_map = [
         version: "0.11.3",
         toPort: 9021
     },
-    {
-        displayTitle: "klogsrv",
-        description: "Klog server. Runs on port 3232.",
-        fileName: "klogsrv.elf",
-        author: "john-tornblom",
-        projectSource: "https://github.com/ps5-payload-dev/klogsrv",
-        binarySource: "https://github.com/ps5-payload-dev/pacbrew-repo/actions/runs/14012252230",
-        version: "0.5.3",
-        toPort: 9021
-    },
-    {
-        displayTitle: "shsrv",
-        description: "Telnet shell server. Runs on port 2323.",
-        fileName: "shsrv.elf",
-        author: "john-tornblom",
-        projectSource: "https://github.com/ps5-payload-dev/shsrv",
-        binarySource: "https://github.com/ps5-payload-dev/shsrv/releases/tag/v0.15",
-        version: "0.15",
-        toPort: 9021
-    },
-    {
-        displayTitle: "gdbsrv",
-        description: "GDB server. Runs on port 2159.",
-        fileName: "gdbsrv.elf",
-        author: "john-tornblom",
-        projectSource: "https://github.com/ps5-payload-dev/gdbsrv",
-        binarySource: "https://github.com/ps5-payload-dev/pacbrew-repo/actions/runs/14012252230",
-        version: "0.5",
-        toPort: 9021
-    },
-    {
-        displayTitle: "ps5debug",
-        description: "Debugger (Experimental beta)",
-        fileName: "ps5debug_v1.0b3.elf",
-        author: "SiSTR0, ctn123",
-        projectSource: "https://github.com/GoldHEN/ps5debug",
-        binarySource: "https://github.com/GoldHEN/ps5debug/releases/download/1.0b3/ps5debug_v1.0b3.elf",
-        version: "1.0b3",
-        supportedFirmwares: ["3.", "4.", "5."],
-        toPort: 9021
-    },
-    {
-        displayTitle: "ps5debug",
-        description: "Debugger, open source version by DizzRL",
-        fileName: "ps5debug_dizz.elf",
-        author: "Dizz, astrelsky, John Tornblom, SiSTR0, golden, idlesauce",
-        projectSource: "https://github.com/idlesauce/ps5debug",
-        binarySource: "https://github.com/idlesauce/ps5debug/releases/download/v0.0.1/ps5debug.elf",
-        version: "0.0.1-r2",
-        toPort: 9021
-    },
-    {
-        displayTitle: "ps5-versions",
-        description: "Shows kernel build, os and sdk versions",
-        fileName: "ps5-versions.elf",
-        author: "SiSTRo",
-        projectSource: "https://github.com/SiSTR0/ps5-versions",
-        binarySource: "https://github.com/SiSTR0/ps5-versions/releases/download/v1.0/ps5-versions.elf",
-        version: "1.0",
-        supportedFirmwares: ["1.", "2.", "3.", "4."]
-    },
-    {
-        displayTitle: "ps5-remoteplay-get-pin",
-        description: "Get Remote Play PIN for offline activated users. Send again to cancel.",
-        fileName: "rp-get-pin.elf",
-        author: "idlesauce",
-        projectSource: "https://github.com/idlesauce/ps5-remoteplay-get-pin",
-        binarySource: "https://github.com/idlesauce/ps5-remoteplay-get-pin/releases/tag/v0.1.1",
-        version: "0.1.1",
-        toPort: 9021
-    },
+    
     {
         // https://github.com/Storm21CH/PS5_Browser_appCache_remove
         displayTitle: "REMOVE CACHE AND CLEAN BROWSER",
