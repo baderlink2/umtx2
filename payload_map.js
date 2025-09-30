3be14731b8b719d645d18a74ca3589d3158670c7
baderlink2 <bouazzaelharyzy@gmail.com>
2025-09-30 13:59:15 +0200

    Update payload_map.js

--- payload_map.js
+++ payload_map.js
@@ -41,10 +41,10 @@ const payload_map = [
         toPort: 9021
     },
     {
-        displayTitle: "ETAHEN V2.4 BETA",
+        displayTitle: "ETAHEN V2.2B",
         description: "FOR 1.XX TO 5.XX",
         fileName: "etaHEN.bin",
-        author: "Last UPDATE 26-09-2025",
+        author: "Last UPDATE 11-05-2025",
         projectSource: "https://github.com/etaHEN/etaHEN",
         binarySource: "https://github.com/etaHEN/etaHEN/blob/360e7c0d2dddf4f4645599d51d652e707c0b717b/etaHEN-2.0b.bin",
         version: "2.1B",
@@ -141,6 +141,7 @@ const payload_map = [
     }
 
 ];
+
 
 
 
