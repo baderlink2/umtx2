13f1140ed86ef8be1f40c5504ca357900832ee21
baderlink2 <bouazzaelharyzy@gmail.com>
2026-04-14 15:13:12 +0200

    Refactor payload_map.js for consistency and updates

--- payload_map.js
+++ payload_map.js
@@ -41,13 +41,13 @@ const payload_map = [
         toPort: 9021
     },
     {
-        displayTitle: "ETAHEN V2.4B",
-        description: "FOR 1.XX TO 10.1",
-        fileName: "etaHEN22b.bin",
+        displayTitle: "HEN",
+        description: "FOR 3.XX 4.XX",
+        fileName: "hen.bin",
         author: "Last UPDATE 02-12-2025",
         projectSource: "https://github.com/etaHEN/etaHEN",
         binarySource: "https://github.com/etaHEN/etaHEN/blob/360e7c0d2dddf4f4645599d51d652e707c0b717b/etaHEN-2.0b.bin",
-        version: "2.3B",
+        version: "V1.0",
         toPort: 9021
     },
 
